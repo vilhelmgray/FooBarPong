4f679fd9dd67427c2b4ed9819e88b35be7879bd5
William Breathitt Gray <vilhelm.gray@gmail.com>
2014-08-02 18:13:01 -0400

    Quit game on ESCAPE key press or SDL_Quit event.

--- src/foobarpong.c
+++ src/foobarpong.c
@@ -52,6 +52,10 @@ int main(void){
                 while(SDL_PollEvent(&event)){
                         switch(event.type){
                                 case SDL_KEYDOWN:
+                                        if(event.key.keysym.sym == SDLK_ESCAPE){
+                                                running = 0;
+                                        }
+                                        break;
                                 case SDL_QUIT:
                                         running = 0;
                                         break;
